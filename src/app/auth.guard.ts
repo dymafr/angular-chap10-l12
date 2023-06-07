235fd7e7b7e38b449a1ea4cfc1b81404eaccc680
dymafr <32801166+dymafr@users.noreply.github.com>
2023-06-07 19:19:29 +0200

    4 files modified, 2 added

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,36 +1,15 @@
-import { Injectable } from "@angular/core";
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  CanActivateChild,
-  CanDeactivate,
-  RouterStateSnapshot
-} from "@angular/router";
-import { UserEditComponent } from "./user-edit/user-edit.component";
+import { inject } from '@angular/core';
+import { CanActivateFn, CanActivateChildFn } from '@angular/router';
+import { AuthService } from './auth.service';
 
-@Injectable({ providedIn: "root" })
-export class AuthGuard
-  implements CanActivate, CanActivateChild, CanDeactivate<UserEditComponent> {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean {
-    console.log(route, state);
-    return true;
-  }
+export const authGuard: CanActivateFn = (route, state) => {
+  console.log(route, state);
+  const authService = inject(AuthService);
+  return authService.isLoggedIn();
+};
 
-  canActivateChild(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean {
-    return true;
-  }
-
-  canDeactivate(
-    component: UserEditComponent,
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ) {
-    return component.canDeactivate();
-  }
-}
+export const authGuardChild: CanActivateChildFn = (route, state) => {
+  console.log(route, state);
+  const authService = inject(AuthService);
+  return authService.isLoggedIn();
+};
